89ba81115a867cf4286002eb8e616700f37e7a96
tokaa1 <premieringsalt@protonmail.com>
2025-04-28 19:26:23 -0400

    button animation

--- src/components/SimpleButton.tsx
+++ src/components/SimpleButton.tsx
@@ -12,7 +12,7 @@ export default function SimpleButton({ children, className, disabled, onClick }:
       onClick={onClick} 
       onMouseEnter={onMouseEnter} 
       onMouseLeave={onMouseLeave} 
-      className={`cursor-pointer h-[16px] text-xs text-center bg-neutral-800/50 hover:bg-neutral-800/80 border-white/10 border-[1px] border-solid font-sans font-bold text-white px-3 py-2 rounded-md ${className} ${disabled ? 'opacity-50' : ''}`}
+      className={`cursor-pointer transition-all duration-100 h-[16px] text-xs text-center bg-neutral-800/50 hover:bg-neutral-800/80 border-white/10 border-[1px] border-solid font-sans font-bold text-white px-3 py-2 rounded-md ${className} ${disabled ? 'opacity-50' : ''}`}
     >
       {children}
     </div>
